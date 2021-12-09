fc852273679c88fe292c8e55a1754c01d2c398b1
Martijn <mkuntzelaers@gmail.com>
2021-12-09 15:26:27 +0100

    Beunen

--- Exergames/Assets/ChestScript.cs
+++ Exergames/Assets/ChestScript.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChestScript : MonoBehaviour
 {
     private Animator anim;
-    
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
     }
-
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        anim.Play("ChestAnimation");
-        Destroy(gameObject, 1f);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            anim.Play("ChestAnimation");
+            Destroy(gameObject, 1f);
+
+            SceneManager.LoadScene("Menu");
+        }
     }
 }
