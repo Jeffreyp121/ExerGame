10ce7b4c3197e78c8e9c588eddc87bad6990179e
Martijn <mkuntzelaers@gmail.com>
2022-01-11 14:46:26 +0100

    Werkend met Gooien hoofd menu
    test

--- Exergames/Assets/mainMenu.cs
+++ Exergames/Assets/mainMenu.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class mainMenu : MonoBehaviour
+public class mainMenu : MonoBehaviour, I_SmartwallInteractable
 {
+    Button start;
     public void PlayGame ()
     {
         SceneManager.LoadScene("LevelKeuze");
@@ -13,5 +15,11 @@ public class mainMenu : MonoBehaviour
     public void StopGame ()
     {
         Application.Quit();
+    }
+
+    public void Hit(Vector3 location)
+    {
+        start = gameObject.GetComponent<Button>();
+        start.onClick.Invoke();
     }
 }
